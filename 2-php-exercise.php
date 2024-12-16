8633d73541003058cd2fa46ec954cf428eea4766
Lemuel <lemuelpuglisi001@gmail.com>
2024-12-16 23:32:14 +0100

    feat: add exercise

---
+++ 2-php-exercise.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Si scriva una funzione, in PHP, che dato in input: una stringa, calcoli e restituisca 
+il numero di vocali presenti nella stringa (sia maiuscole che minuscole). La funzione 
+deve iterare manualmente sui caratteri della stringa e utilizzare la funzione built-in 
+`in_array()` per verificare se un carattere è una vocale. La funzione dovrà avere nome 
+`contaVocali`, ed accettare come parametro `$str` per la stringa di input. Si precisa 
+che la risposta deve iniziare e terminare con i tag del codice PHP.
+*/
+
+function contaVocali($str) {
+    // ...
+}
+
+// test
+echo contaVocali('Ciao Mondo'); // 4
